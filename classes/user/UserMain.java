8e807f5a520d0c1390a393ccfd0ac2fdf3b3615d
Elizabeth Perk <K33TY@users.noreply.github.com>
2017-10-27 01:18:04 -0400

    not sure why UserMain does not have syntax highlighting

---
+++ classes/user/UserMain.java
@@ -0,0 +1,19 @@
+package user;
+
+import fabric.lang.*;
+import fabric.lang.security.*;
+import fabric.worker.*;
+import fabric.worker.remote.*;
+import java.lang.*;
+import javax.servlet.ServletException;
+import sif.html.*;
+import sif.servlet.Action;
+import sif.servlet.Request;
+import sif.servlet.HTMLRequest;
+import sif.servlet.Servlet;
+import fabric.util.Map;
+import login.*;
+
+public class FrontPage extends login.AuthenticatedAction {
+
+}
