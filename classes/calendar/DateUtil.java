c5e4da146fbc556947a8773e59eb5280802ee84f
Elizabeth Perk <K33TY@users.noreply.github.com>
2017-10-29 17:05:48 -0400

    added Date.fab to fabric.util package, and added runtime methods, so now can have this Date Util Class in fabric version of calendar

--- classes/calendar/DateUtil.java
+++ classes/calendar/DateUtil.java
@@ -1,5 +1,16 @@
 package calendar;
 
+/**
+ *
+ * A class configuring dates and time used within the calendar:
+ * Date and time collected upon class instantiation:
+ *    * for worker nodes, instantiation starts on authentication of user.
+ *    * for store nodes, instantiation upon creation of store.
+ *  - Assumption: Avoidance of any covert channel of a worker knowing another worker's system time,
+ *                by only allowing given worker access to their own system time
+ *
+**/
+
 public class DateUtil
 {
 
